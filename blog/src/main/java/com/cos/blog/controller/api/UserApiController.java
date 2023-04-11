cc2a05efbe9723e69cf39a0692442c86c9fbe025
dnrms47 <dnrms47@naver.com>
2023-04-11 16:15:50 +0900

    회원가입 개발완료!

--- blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,28 +1,38 @@
 package com.cos.blog.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
 
 // 얘는 나중에 웹에도 쓸수 있다.
 //데이터만 리턴을 해주는 컨트롤러 이기 때문에 @RestController
 @RestController						
 public class UserApiController {
+
+		// DI ( Spring이 컴포넌트 스캔할 때 스프링 빈을 통해서 IoC에 띄어준다 )
+		@Autowired
+		private UserService userService;
 	
 		// JSON이니까 @RequestBody로 파라미터 받음
 		// 통신상태를 확인하기 위해 HttpStatus.OK 
 		@PostMapping("/api/user")
-		public ResponseDto<Integer> save(@RequestBody User user) {
+		public ResponseDto<Integer> save(@RequestBody User user) {		// username, password, email
 			System.out.println("UserApiController : save 호출됨!");
 			
-			// 실제로 여기서 DB에 insert를 하고 아래에서 return이 되면 되요.
+			// role은 직접 넣어줘야함
+			user.setRole(RoleType.USER);
 			
+			// 실제로 여기서 DB에 insert를 하고 아래에서 return이 되면 되요.
+			int result = userService.회원가입(user);
 			
 			// 회원가입이 성공 시 [ 상태값 : 200, 1 ] 호출
-			return new ResponseDto<Integer>(HttpStatus.OK, 1);	// 자바 오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+			return new ResponseDto<Integer>(HttpStatus.OK, result);	// 자바 오브젝트를 JSON으로 변환해서 리턴 (Jackson)
 		}
 }
---
+++ blog/src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,34 @@
+package com.cos.blog.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+
+// 스프링이 컴포넌트 스캔을 통해서 빈에 등록을 해준다.  ( IoC를 해준다 ) 메모리를 대신 띄어준다.
+@Service
+public class UserService {
+	
+		@Autowired
+		private UserRepository userRepository;
+		
+		// javax의 @Transaction
+		// 회원가입 함수의 전체가 하나의 트랜잭션이 된다. 전체가 실행이되면 그때 커밋이 될꺼구요
+		// 트랜잭션중 하나라도 실패를 한다면 커밋이 되지 않고 RollBack이 될겁니다.
+		@Transactional
+		public int 회원가입(User user) {
+			try {
+				// userRepository 하나의 트랜잭션
+				userRepository.save(user);
+				return 1;
+			} catch (Exception  e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				System.out.println("UserService : 회원가입() : " + e.getMessage());
+			}
+			return -1;
+		}
+}
